3510f663a3e24211bd4caa710dd17e66c7bcdf1b
biditadas0-code <biditadas0@gmail.com>
2026-09-01 08:41:52 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -72,7 +72,7 @@ def chat():
         "role": "user",
         "content": user_message
     })
-    conversation_history = conversation_history[-6:]
+    conversation_history[:] = conversation_history[-6:]
 
     try:
 
